a3901c7a8e44d0abe0006c0b820e6642f447a7a7
phoenix <mail@kundeng.us>
2026-08-08 13:27:10 -0400

    Try it (#1)
    Reviewed-on: http://git.kundeng.us/phoenix/labyrinth/pulls/1

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 pub mod config;
 
-#[derive(Default)]
+use aws_sdk_s3::config::{Credentials, Region};
+
+#[derive(Debug, Default)]
 pub struct Data {
     pub filepath: String,
     pub raw_data: Vec<u8>,
@@ -14,6 +16,32 @@ pub async fn load_data(filepath: &str) -> Result<Vec<u8>, std::io::Error> {
     tokio::fs::read(filepath).await
 }
 
+pub async fn init_client(config: &config::Config) -> aws_sdk_s3::Client {
+    let credentials = Credentials::new(
+        config.access_key_id.clone(),
+        config.secret_key.clone(),
+        None,
+        None,
+        "maze",
+    );
+
+    let region = Region::new(config.region.to_string());
+    let config = aws_config::from_env()
+        .region(region)
+        .credentials_provider(credentials)
+        .endpoint_url(config.url.clone())
+        .load()
+        .await;
+
+    let s3_config = aws_sdk_s3::config::Builder::from(&config)
+        .force_path_style(true)
+        .build();
+
+    let client = aws_sdk_s3::Client::from_conf(s3_config);
+    client
+}
+
+#[derive(Debug)]
 pub enum Error {
     Info(String),
     SError(aws_sdk_s3::operation::put_object::PutObjectError),
@@ -25,9 +53,7 @@ impl Labyrinth {
         file_key: &str,
         data: &Data,
     ) -> Result<aws_sdk_s3::operation::put_object::PutObjectOutput, Error> {
-        let config = aws_config::load_from_env().await;
-        let client = aws_sdk_s3::Client::new(&config);
-
+        let client = init_client(&self.config).await;
         let data_content = if data.raw_data.is_empty() {
             match load_data(&data.filepath).await {
                 Ok(content) => content,
